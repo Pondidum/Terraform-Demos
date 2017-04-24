acd31141e5b9d635bd8ff20e74175f0b801fccc1
andy <andy@andydote.co.uk>
2017-04-24 18:46:47 +0300

    s3-multi: move lookup to the main tf file

--- s3-static-hosting-multifile/files.sh
+++ s3-static-hosting-multifile/files.sh
@@ -3,19 +3,7 @@
 SRC="src/"
 TF_FILE="files.tf"
 
-cat > $TF_FILE << EOM
-variable "mime_types" {
-  default = {
-    htm = "text/html"
-    html = "text/html"
-    css = "text/css"
-    js = "application/javascript"
-    map = "application/javascript"
-    json = "application/json"
-  }
-}
-EOM
-
+cat > $TF_FILE ''
 
 COUNT=0
 find $SRC -iname '*.*' | while read path; do
--- s3-static-hosting-multifile/main.tf
+++ s3-static-hosting-multifile/main.tf
@@ -1,3 +1,14 @@
+variable "mime_types" {
+  default = {
+    htm = "text/html"
+    html = "text/html"
+    css = "text/css"
+    js = "application/javascript"
+    map = "application/javascript"
+    json = "application/json"
+  }
+}
+
 variable "bucket" {
   default = "examplebucket"
 }
